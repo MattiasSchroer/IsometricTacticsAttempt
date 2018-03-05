8d65d7c08352cb9cc6f935a85bdd6802397d16c5
brmarco <bmarco@unb.ca>
2018-03-05 17:25:11 -0400

    Commit1

--- Assets/Scripts/LockRot.cs
+++ Assets/Scripts/LockRot.cs
@@ -3,16 +3,42 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LockRot : MonoBehaviour {
+    // Use this for initialization
+    void Start () {
 
-	// Use this for initialization
-	void Start () {
-		
 	}
 	
 	float lockPos = 0;
-
+    
 	void Update()
 	{
-		transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 45, lockPos);
-	}
+        float speed = 10;
+        float rotSpeed = 30;
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, rotSpeed * Time.deltaTime, 0, Space.World);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, -rotSpeed * Time.deltaTime,0, Space.World);
+        }
+
+        //transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, 45, lockPos);
+    }
 }
