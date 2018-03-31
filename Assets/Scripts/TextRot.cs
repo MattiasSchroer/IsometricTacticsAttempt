8f99cc9f761d029dfafd933d3ca8eb4cb834af55
brmarco <bmarco@unb.ca>
2018-03-31 16:42:19 -0300

    Commit5

---
+++ Assets/Scripts/TextRot.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextRot : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotSpeed = 30;
+        if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, rotSpeed * Time.deltaTime, 0, Space.World);
+        }
+        if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, -rotSpeed * Time.deltaTime, 0, Space.World);
+        }
+    }
+}
